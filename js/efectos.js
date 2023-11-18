be3adf6b4f778bfef517815053438a67e52b6298
Facundo Rodríguez <facundorodriguez7714@gmail.com>
2023-11-17 21:34:12 -0300

    Barra de navegacion, bootstrap

---
+++ js/efectos.js
@@ -0,0 +1,17 @@
+
+/* Muestra cartas de "aptitudes principales" en index**/
+
+function mostrar(){
+    document.getElementById('seccionAptitudes').innerHTML = `<div class="cartas">
+    <button class="carta" ">
+        <h3>JavaScript</h3>
+    </button>
+    <button class="carta">
+        <h3>React</h3>
+    </button>
+    <button class="carta">
+        <h3>Git</h3>
+    </button>
+</div>`
+}
+
